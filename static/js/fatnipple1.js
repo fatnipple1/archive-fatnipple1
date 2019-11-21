bff1cefad8b5785900ce0c221ca2bb2288aca6df
William Gardner <539362@cuhsd-int.prv>
2019-11-21 13:50:57 -0800

    fatnp change image format

--- static/js/fatnipple1.js
+++ static/js/fatnipple1.js
@@ -2,7 +2,8 @@ var N_titles = 4
 var N_homes = 29
 
 var ref = {
-    2: 'waterfountain.html'
+    15: 'waterfountain.html',
+    13: 'romil.html'
 }
 
 function randInt(min, max) {
@@ -13,4 +14,4 @@ $('#title').attr('src', `static/img/title/title (${randInt(0, N_titles)+1}).png`
 
 var home_num = randInt(0, N_homes) + 1
 if (ref[home_num]) $('#entry').attr('href', ref[home_num])
-$('#home').attr('src', `static/img/home/home (${home_num}).png`)
+$('#home').attr('src', `static/img/home/home (${home_num}).jpg`)
