5fe0a5673c47252f9a8d40ddbd9c23fc9d128d0c
William Gardner <wg4568@gmail.com>
2019-11-21 11:14:45 -0800

    fatnipple waterfountain

---
+++ static/js/fatnipple1.js
@@ -0,0 +1,16 @@
+var N_titles = 4
+var N_homes = 29
+
+var ref = {
+    2: 'waterfountain.html'
+}
+
+function randInt(min, max) {
+    return min + Math.floor(Math.random() * (max - min))
+}
+
+$('#title').attr('src', `static/img/title/title (${randInt(0, N_titles)+1}).png`)
+
+var home_num = randInt(0, N_homes) + 1
+if (ref[home_num]) $('#entry').attr('href', ref[home_num])
+$('#home').attr('src', `static/img/home/home (${home_num}).png`)
